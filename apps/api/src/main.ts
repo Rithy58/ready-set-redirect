e1e8f45cf60dacfbbde196cc834636ba50d5710f
Rithy Muth <Rithy58@users.noreply.github.com>
2022-03-15 15:51:01 -0700

    Fixed heroku

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as fs from 'fs';
 import { Message } from '@ready-set-redirect/api-interfaces';
 import { Client } from 'pg';
 import { postLink } from './app/postLink';
@@ -28,8 +29,11 @@ app.post('/api/post', postLink(client))
 
 app.get('/api/get/:id', getLink(client))
 
-const port = process.env.PORT || 3333;
+const port = process.env.port || 3333;
 const server = app.listen(port, () => {
+  if (process.env.DYNO) {
+    fs.openSync('/tmp/app-initialized', 'w');
+  }
   console.log('Listening at http://localhost:' + port + '/api');
 });
 server.on('error', console.error);
