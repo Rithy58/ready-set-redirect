4ffd1d0694990ed36364d85fa9e90c0af21764b2
Rithy Muth <Rithy58@users.noreply.github.com>
2022-03-15 11:58:34 -0700

    initial commit

---
+++ apps/ready-set-redirect/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'ready-set-redirect',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/ready-set-redirect',
+};
