b4de599fd2050ab865b422229b5617eb3fcdc0c2
Rithy Muth <Rithy58@users.noreply.github.com>
2022-03-18 12:49:59 -0700

    Reformatted getLink query

--- apps/api/src/app/getLink.ts
+++ apps/api/src/app/getLink.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express'
 import { Client } from 'pg'
 
 export const getLink = (client: Client) => (req: Request, res: Response) => {
-  const query = `SELECT url, timer FROM data WHERE id = $1`
+  const query = `
+    SELECT url, timer
+    FROM data
+    WHERE id = $1
+  `
   const values = [+req.params.id]
   client.query(query, values, (err, data) => {
     if(err) {
