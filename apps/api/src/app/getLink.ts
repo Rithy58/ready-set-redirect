8cb5b4b389a84b934c1105ff852df2a6f8c62a0d
Rithy Muth <Rithy58@users.noreply.github.com>
2022-03-18 12:48:38 -0700

    Log error and clean up getLink

--- apps/api/src/app/getLink.ts
+++ apps/api/src/app/getLink.ts
@@ -1,4 +1,3 @@
-import { LinkData } from '@ready-set-redirect/api-interfaces'
 import { Request, Response } from 'express'
 import { Client } from 'pg'
 
@@ -6,6 +5,9 @@ export const getLink = (client: Client) => (req: Request, res: Response) => {
   const query = `SELECT url, timer FROM data WHERE id = $1`
   const values = [+req.params.id]
   client.query(query, values, (err, data) => {
+    if(err) {
+      console.error(err)
+    }
     res.json(data.rows[0])
   })
 }
